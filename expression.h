381d510a539d55b3e83442bcc267b28567d50a3a
Liu Cong <liucong@pingcap.com>
2017-09-27 20:59:53 +0800

    Detail interface of expression.h

--- expression.h
+++ expression.h
@@ -1,18 +1,140 @@
 #pragma once
 
-#include "rapidjson/document.h"
+#include <string>
+#include <vector>
 
-namespace ptio {
+using std::string;
+using std::vector;
 
 struct Expression {
-    using Dict = const rapidjson::Document;
+    using PropType = int;
+    using PropValInt = uint32_t;
+    using PropValFloat = float;
 
-    Expression(const char *in) {
+    enum {
+        PropNone = -1,
+        PropString = 0,
+        PropInt = 1,
+        PropFloat = 2,
+    };
+
+    using CmpOp = int;
+
+    enum {
+        Eq = 0,
+        Ge = 1,
+        Le = 2,
+        Gt = 3,
+        Lt = 4,
+    };
+
+    CmpOp cmp_op;
+
+    string name;
+    PropType type;
+
+    PropValInt val_int;
+    PropValFloat val_float;
+    string val_string;
+
+    template <typename T>
+    inline static bool ExeEq(const T &a, const T &b) {
+        return a == b;
+    }
+    template <typename T>
+    inline static bool ExeGe(const T &a, const T &b) {
+        return a >= b;
+    }
+    template <typename T>
+    inline static bool ExeLe(const T &a, const T &b) {
+        return a <= b;
+    }
+    template <typename T>
+    inline static bool ExeGt(const T &a, const T &b) {
+        return a > b;
+    }
+    template <typename T>
+    inline static bool ExeLt(const T &a, const T &b) {
+        return a < b;
+    }
+    template <typename T>
+    inline static bool Cmp(const T &a, CmpOp op, const T &b) {
+        switch (op) {
+        case Eq:
+            return Expression::ExeEq(a, b);
+        case Ge:
+            return Expression::ExeGe(a, b);
+        case Le:
+            return Expression::ExeLe(a, b);
+        case Gt:
+            return Expression::ExeGt(a, b);
+        case Lt:
+            return Expression::ExeLt(a, b);
+        default:
+            return false;
+        }
     }
 
-    bool Matched(const Dict &dict) {
-        return false;
+    template <typename iteratable>
+    inline bool Matched(iteratable props) {
+        for (auto it = props.begin(); it != props.end(); ++it) {
+            if (type != it->Type() || name != it->Name())
+                continue;
+            if (it->Type() == PropString)
+                return Expression::Cmp(val_string, cmp_op, string(it->String()));
+            else if (it->Type() == PropInt)
+                return Expression::Cmp(val_int, cmp_op, it->Int());
+            else if (it->Type() == PropFloat)
+                return Expression::Cmp(val_float, cmp_op, it->Float());
+            else
+                return false;
+        }
+        return true;
     }
+
+    inline Expression(const char *name_, CmpOp cmp_op_, const char *val_) :
+        cmp_op(cmp_op_), name(name_), type(PropString), val_string(val_) {}
+
+    inline Expression(const char *name_, CmpOp cmp_op_, PropValInt val_) :
+        cmp_op(cmp_op_), name(name_), type(PropInt), val_int(val_) {}
+
+    inline Expression(const char *name_, CmpOp cmp_op_, PropValFloat val_) :
+        cmp_op(cmp_op_), name(name_), type(PropFloat), val_float(val_) {}
 };
 
-}
+struct Expressions : public vector<Expression> {
+    using Self = vector<Expression>;
+
+    using CmpOp = Expression::CmpOp;
+
+    using PropValInt = Expression::PropValInt;
+    using PropValFloat = Expression::PropValFloat;
+
+    inline Expressions & And(const char *name, CmpOp cmp, const char *val) {
+        Self::emplace_back(name, cmp, val);
+        return *this;
+    }
+
+    inline Expressions & And(const char *name, CmpOp cmp, PropValInt val) {
+        Self::emplace_back(name, cmp, val);
+        return *this;
+    }
+
+    inline Expressions & And(const char *name, CmpOp cmp, PropValFloat val) {
+        Self::emplace_back(name, cmp, val);
+        return *this;
+    }
+
+    template <typename iteratable>
+    inline bool Matched(iteratable props) {
+        for (auto it = Self::begin(); it != Self::end(); ++it)
+            if (!it->Matched(props))
+                return false;
+        return true;
+    }
+
+    inline void Parse(const char *in) {
+        // TODO: parse
+        // And("brand", Eq, "Apple").And("price", Lt, 3000);
+    }
+};
--- main.cpp
+++ main.cpp
@@ -2,18 +2,73 @@
 #include "rapidjson/document.h"
 #include "expression.h"
 
-using namespace ptio;
+struct Dict {
+    using Json = rapidjson::Document;
+
+    Json &doc;
+
+    Dict(Json &doc_) : doc(doc_) {}
+
+    struct iterator {
+        using It = rapidjson::Value::ConstMemberIterator;
+
+        It it;
+
+        iterator(const It &it_) : it(it_) {}
+
+        bool operator != (const iterator &x) {
+            return it != x.it;
+        }
+        void operator ++ () {
+            ++it;
+        }
+        iterator* operator -> () {
+            return this;
+        }
+        Expression::PropType Type() {
+            auto type = it->value.GetType();
+            if (type == rapidjson::kStringType)
+                return Expression::PropString;
+            if (type == rapidjson::kNumberType && it->value.IsDouble())
+                return Expression::PropFloat;
+            if (type == rapidjson::kNumberType)
+                return Expression::PropInt;
+            return Expression::PropNone;
+        }
+        const char * Name() {
+            return it->name.GetString();
+        }
+        const char * String() {
+            return it->value.GetString();
+        }
+        Expression::PropValInt Int() {
+            return (Expression::PropValInt)it->value.GetInt();
+        }
+        Expression::PropValFloat Float() {
+            return (Expression::PropValFloat)it->value.GetDouble();
+        }
+    };
+
+    iterator begin() const {
+        return iterator(doc.MemberBegin());
+    }
+
+    iterator end() const {
+        return iterator(doc.MemberEnd());
+    }
+};
 
 int main() {
     const char *expression = "(brand = 'Apple' & price > 6000) | (brand = 'HW' & price > 5000)";
     const char *data = "{\"brand\": \"Apple\", \"price\": 8888.8.8}";
 
-    ptio::Expression exp(expression);
+    Expressions exp;
+    exp.Parse(expression);
 
     rapidjson::Document row;
     row.Parse(data);
 
-    bool matched = exp.Matched(row);
+    bool matched = exp.Matched(*(Dict*)(&row));
     std::cout << "{" << data << "} " << (matched ? "" : "not") << " matched {" << expression << "}" << std::endl;
 
     return 0;
